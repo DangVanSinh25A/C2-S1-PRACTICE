7b5355e7c794617fde45f97500db69d81ea24c22
sinh <sinhdangvan27@gmail.com>
2024-05-05 16:17:10 +0700

    C2-S1-PRACTICE

---
+++ exercise-2/src/App.jsx
@@ -0,0 +1,32 @@
+//EXERCISE 2 _ q2.1: 
+
+      // import React from "react";
+
+      // function App() {
+      //   return (
+      //     <>
+      //       <p>Hello</p>
+      //     </>
+      //   );
+      // }
+
+      // export default App;
+
+//EXERCISE 2 _ q2.2: 
+
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
